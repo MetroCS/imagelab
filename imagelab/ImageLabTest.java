a62d4d2a8ab817a686d7f72ee0835179bfb02986
alope151 <alope151@msudenver.edu>
2019-12-04 21:34:22 -0700

    fixed 2 lines of code to make window dynamic

---
+++ imagelab/ImageLabTest.java
@@ -0,0 +1,55 @@
+package imagelab;
+
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test ImageLab.
+ *
+ * @author  Conlan, Mbami, Antonio
+ * @version 2019.11.18
+ */
+public class ImageLabTest
+{
+    /**
+     * Default constructor for test class ImageLabTest
+     */
+    public ImageLabTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @Test
+    public void testPause()
+    {
+        ImageLab imageLab = new ImageLab();
+        
+        //given audio is playing
+        //after pause menu item is pressed
+        //audio stops playing
+        
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
